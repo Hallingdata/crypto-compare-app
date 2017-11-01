18579f49a1f830b255ded868c588948f8e6c01c4
Asgeir Sognefest <sogasg@gmail.com>
2017-11-01 20:14:37 +0100

    Main view working

--- src/components/CoinList.tsx
+++ src/components/CoinList.tsx
@@ -1,15 +1,33 @@
-import { Container, Content, Header, List, ListItem, Text } from "native-base"
+import {
+  Container,
+  Content,
+  Header,
+  List,
+  ListItem,
+  Text,
+  Right,
+  Left,
+  Body,
+  Badge,
+  Thumbnail,
+} from "native-base"
+import { Image, View } from "react-native"
 import * as R from "ramda"
 import * as React from "react"
 
 import { schema } from "../data/cryptoCompare.schema"
 import { GraphqlHOC } from "../data/Graphql.HOC"
+import { RefreshControl } from "react-native"
 
 const coinsQuery = `
 { 
-  coins { 
-    CoinName
-    Id
+  coins(limit: 100) {
+    Name
+    long
+    cap24hrChange
+    mktcap
+    ImageUrl
+    price
  }
 }`
 
@@ -20,8 +38,46 @@ const coinListRaw = ({ data }: { data: { coins: Array<Coin> } }) => (
       <List>
         {R.map(coin => {
           return (
-            <ListItem key={coin.Id}>
-              <Text>{coin.CoinName}</Text>
+            <ListItem key={coin.long + coin.mktcap}>
+              <Left style={{flexGrow: 2}}>
+                <Thumbnail
+                  style={{ width: 30, height: 30 }}
+                  source={{
+                    uri: "https://www.cryptocompare.com" + coin.ImageUrl,
+                  }}
+                />
+                <Text>{coin.long} </Text>
+              </Left>
+              <Body style={{flexGrow: 1}}>
+                {coin.cap24hrChange >= 0 ? (
+                  <Badge
+                    style={{
+                      backgroundColor: `rgba(115,181,102,${coin.cap24hrChange *
+                        4 /
+                        100})`,
+                    }}
+                  >
+                    <Text style={{ color: "black" }}>
+                      + {coin.cap24hrChange}%
+                    </Text>
+                  </Badge>
+                ) : (
+                  <Badge
+                    style={{
+                      backgroundColor: `rgba(218,59,51,${coin.cap24hrChange *
+                        -4 /
+                        100})`,
+                    }}
+                  >
+                    <Text style={{ color: "black" }}>
+                      {coin.cap24hrChange}%
+                    </Text>
+                  </Badge>
+                )}
+              </Body>
+              <Right style={{flexGrow: 1}}>
+                <Text>${coin.price}</Text>
+              </Right>
             </ListItem>
           )
         }, data.coins)}
@@ -33,6 +89,10 @@ const coinListRaw = ({ data }: { data: { coins: Array<Coin> } }) => (
 export const CoinList = GraphqlHOC(coinListRaw, schema, coinsQuery)
 
 type Coin = {
-  CoinName: string
-  Id: number
+  long: string
+  cap24hrChange: number
+  mktcap: number
+  Name: string
+  ImageUrl: string
+  price: string
 }
--- src/data/cryptoCompare.schema.ts
+++ src/data/cryptoCompare.schema.ts
@@ -4,50 +4,94 @@ import * as R from "ramda"
 
 const typeDefs = `
 type Coin {
-  Id: Int!
-  Symbol: String
-  ImageUrl: String
-  Name: String
-  CoinName: String
+  Id: String!
+  Symbol: String!
+  ImageUrl: String!
+  Name: String!
+  SortOrder: String!
+  CoinName: String!
   candleHour: [DataPoint]
+  cap24hrChange: String!
+  long: String!
+  mktcap: Float!
+  perc: Float!
+  price: Float!
+  shapeshift: Boolean!
+  short: String!
+  supply: Int!
+  usdVolume: Int!
+  volume: Int!
+  vwapData: Float!
+  vwapDataBTC: Float!
 }
 type DataPoint {
-  time: Int
-  close: Float
-  high: Float 
-  low: Float
-  open: Float
-  volumefrom: Float
-  volumeto: Float
+  time: Int!
+  close: Float!
+  high: Float!
+  low: Float!
+  open: Float!
+  volumefrom: Float!
+  volumeto: Float!
 }
 type Query {
-  coin(id: Int!): Coin
-  coins: [Coin]
+  coin(id: String!): Coin!
+  coins(limit: Int, sortBy: String): [Coin]!
 }
 schema {
   query: Query
 }
 `
 
-const endpoint = "https://min-api.cryptocompare.com"
-const oldEndpoint = "https://www.cryptocompare.com/api"
+const cryptoCompare = "https://min-api.cryptocompare.com"
+const cryptoCompare_old = "https://www.cryptocompare.com/api"
+const coinCap = "http://coincap.io"
 
 const dataToJSON = R.composeP(R.prop("Data"), R.invoker(0, "json"))
 
 const objectToArray = (data: any) =>
   R.map((key: string) => data[key], R.keys(data))
 
-const coin = async (root: any, { id }: { id: number }) =>
+const coin = async (root: any, { id }: { id: string }) =>
   R.composeP(R.prop("General"), dataToJSON, fetch)(
-    `${oldEndpoint}/data/coinsnapshotfullbyid/?id=${id}`
+    `${cryptoCompare_old}/data/coinsnapshotfullbyid/?id=${id}`
   )
 
-const coins = () =>
-  R.composeP(objectToArray, dataToJSON, fetch)(`${endpoint}/data/all/coinlist`)
+const coins = async (root: any, { limit = 100 }) => {
+  const coinInfoCryptoComparePromise = R.composeP(dataToJSON, fetch)(
+    `${cryptoCompare}/data/all/coinlist`
+  )
+  const topCoinsPromise = R.composeP(
+    R.take(limit),
+    R.invoker(0, "json"),
+    fetch
+  )(`${coinCap}/front`)
+
+  const [coinInfoCryptoCompare, topCoins]: Array<any> = await Promise.all([
+    coinInfoCryptoComparePromise,
+    topCoinsPromise,
+  ])
+  return R.reduce(
+    (acc: any, topCoin: any) => {
+      const ccCoinInfo = coinInfoCryptoCompare[topCoin.short]
+
+      return ccCoinInfo
+        ? R.append(
+            {
+              ...topCoin,
+              ...ccCoinInfo,
+            },
+            acc
+          )
+        : acc
+    },
+    [],
+    topCoins
+  )
+}
 
 const candleHour = ({ Symbol }: { Symbol: string }) =>
   R.composeP(dataToJSON, fetch)(
-    `${endpoint}/data/histohour?fsym=${Symbol}&tsym=USD&limit=10&aggregate=3&e=CCCAGG`
+    `${cryptoCompare}/data/histohour?fsym=${Symbol}&tsym=USD&limit=10&aggregate=3&e=CCCAGG`
   )
 
 const resolvers = {
